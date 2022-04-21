a15f476b94cccc5613d66562d4040a89f7df4585
rex-smith <rex.smith17@gmail.com>
2022-04-21 18:59:57 -0400

    Finished project

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,52 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+  end
+
   def new
-    # Method
+    @user = User.new
   end
 
   def create
-    # Method
+    # @user = User.new(user_params)
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new, status: unprocessable_entity
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :username, :password)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   validates :username, :email, :password, presence: true
+  validates :username, :password, length: { in: 8..20 }
 end
